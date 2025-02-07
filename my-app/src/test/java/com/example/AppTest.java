0ea737699e145c251a0b461264d45a40089dcaa7
Azjargal67 <azjargal1627@gmail.com>
2025-02-07 15:39:18 +0800

    mvn project

---
+++ my-app/src/test/java/com/example/AppTest.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for App.
+ */
+public class AppTest {
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(originalOut);
+    }
+
+  @Test
+public void testMainOutput() {
+    App.main(new String[]{});
+
+    assertEquals("Hello World!.", outputStream.toString().trim(), "match 'Hello World!.'");
+  }
+
+}
